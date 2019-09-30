40bf0f6046f1a65be980b5a67335bdcbbaf1c524
William Francois <william.francois@outlook.com>
2019-09-30 18:38:25 +0200

    SqlConnection First Push

---
+++ DataBussiness/SqlConn.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBussiness
+{
+    public class SqlConn
+    {
+        public bool ConnectionTest()
+        {
+            SqlConnection sqlConnection = new SqlConnection("Home");
+            //SqlConnection.
+            return true;
+        }
+    }
+}
---
+++ WebServices/Controllers/UtilitiesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebServices.Controllers
+{
+    public class UtilitiesController : ApiController
+    {
+        DataBussiness.SqlConn sql = new DataBussiness.SqlConn();
+        
+        // GET api/Connection
+        public string Get()
+        {
+            sql.ConnectionTest();
+            return "Database Connection established";
+        }
+
+    }
+}
